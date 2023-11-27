5240e842d669ed71227787e4ee4ced2dac5a28d1
colineckert <colineckert10@gmail.com>
2023-11-26 20:40:53 -0800

    fix post create route data obj

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -23,8 +23,8 @@ export const postRouter = createTRPCRouter({
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
-          createdBy: { connect: { id: ctx.session.user.id } },
+          content: input.name,
+          userId: ctx.session.user.id,
         },
       });
     }),
@@ -32,7 +32,7 @@ export const postRouter = createTRPCRouter({
   getLatest: protectedProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
-      where: { createdBy: { id: ctx.session.user.id } },
+      where: { user: { id: ctx.session.user.id } },
     });
   }),
 
