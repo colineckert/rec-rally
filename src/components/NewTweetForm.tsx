f8e8e60ce792c2bb851a6d3c5e6b017b14ec5d7a
colineckert <colineckert10@gmail.com>
2023-12-15 21:30:14 -0800

    start new tweet form

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,15 @@
+export function NewTweetForm() {
+  return (
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        {/* <ProfileImage src="#url" /> */}
+        <textarea
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's happening?"
+        />
+      </div>
+      <button>asdfa</button>
+    </form>
+  )
+};
+
