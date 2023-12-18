57854973f153c6f2377a90ae84e13734a5fdcb55
colineckert <colineckert10@gmail.com>
2023-12-18 05:43:27 -0800

    handle post create

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -1,50 +1,62 @@
-import { useSession } from "next-auth/react"
-import { useCallback, useLayoutEffect, useRef, useState } from "react"
-import { Button } from "./Button"
-import { ProfileImage } from "./ProfileImage"
+import { useSession } from "next-auth/react";
+import { type FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { Button } from "./Button";
+import { ProfileImage } from "./ProfileImage";
+import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
-  if (textArea == null) return
-  textArea.style.height = '0'
-  textArea.style.height = `${textArea.scrollHeight}px`
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
 }
 
 function Form() {
-  const session = useSession()
-  const [inputValue, setInputValue] = useState('')
-  const textAreaRef = useRef<HTMLTextAreaElement>()
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
   const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
-    updateTextAreaSize(textArea)
-    textAreaRef.current = textArea
-  }, [])
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
 
   useLayoutEffect(() => {
-    updateTextAreaSize(textAreaRef.current)
-  }, [inputValue])
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
 
-  if (session.status !== 'authenticated') return null;
+  const createPost = api.post.create.useMutation({
+    onSuccess: (newPost) => {
+      console.log(newPost);
+      setInputValue('');
+    }
+  });
+
+  if (session.status !== "authenticated") return null;
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    createPost.mutate({ content: inputValue });
+  }
 
   return (
-    <form className="flex flex-col gap-2 border-b px-4 py-2">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
           ref={inputRef}
           style={{ height: 0 }}
-          onChange={e => setInputValue(e.target.value)}
+          onChange={(e) => setInputValue(e.target.value)}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         />
       </div>
-      <Button className="self-end">Tweet</Button>
+      <Button className="self-end">Post</Button>
     </form>
-  )
+  );
 }
 
 export function NewPostForm() {
-  const session = useSession()
-  if (session.status !== 'authenticated') return
+  const session = useSession();
+  if (session.status !== "authenticated") return;
 
-  return <Form />
+  return <Form />;
 }
-
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,36 +7,13 @@ import {
 } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          content: input.name,
-          userId: ctx.session.user.id,
-        },
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input: { content }, ctx }) => {
+      const post = await ctx.db.post.create({
+        data: { content, userId: ctx.session.user.id },
       });
+
+      return post;
     }),
-
-  getLatest: protectedProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-      where: { user: { id: ctx.session.user.id } },
-    });
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
 });
