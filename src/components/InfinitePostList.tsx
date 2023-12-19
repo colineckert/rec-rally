0f05ed553d157991c1cd319b0e53f0c5a70d690d
colineckert <colineckert10@gmail.com>
2023-12-19 07:13:44 -0800

    work on post list items and heart button

--- src/components/InfinitePostList.tsx
+++ src/components/InfinitePostList.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { ProfileImage } from "./ProfileImage";
+import { useSession } from "next-auth/react";
+import { VscHeart, VscHeartFilled } from "react-icons/vsc";
+import { IconHoverEffect } from "./IconHoverEffect";
+
 type Post = {
   id: string;
   content: string;
@@ -14,7 +21,7 @@ type Post = {
 type InfinitePostListProps = {
   isLoading: boolean;
   isError: boolean;
-  hasMore?: boolean;
+  hasMore: boolean;
   fetchNewPosts: () => Promise<unknown>;
   posts?: Post[];
 };
@@ -26,6 +33,104 @@ export function InfinitePostList({
   fetchNewPosts,
   hasMore,
 }: InfinitePostListProps) {
-  console.log(posts);
-  return <div>asdf</div>;
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error...</h1>;
+
+  if (posts == null || posts.length === 0) {
+    return (
+      <h2 className="my-4 text-center text-2xl text-gray-500">No Posts</h2>
+    );
+  }
+
+  return (
+    <ul>
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={fetchNewPosts}
+        hasMore={hasMore}
+        loader={"Loading..."}
+      >
+        {posts.map((post) => {
+          return <PostListItem key={post.id} {...post} />;
+        })}
+      </InfiniteScroll>
+    </ul>
+  );
+}
+
+const dataTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
+
+function PostListItem({
+  id,
+  user,
+  content,
+  createdAt,
+  likeCount,
+  likedByMe,
+}: Post) {
+  return (
+    <li className="flex gap-4 border-b px-4 py-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            href={`/profiles/${user.id}`}
+            className="font-bold hover:underline focus-visible:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dataTimeFormatter.format(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HearButton likedByMe={likedByMe} likeCount={likeCount} />
+      </div>
+    </li>
+  );
+}
+
+type HeartButtonProps = {
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HearButton({ likedByMe, likeCount }: HeartButtonProps) {
+  const session = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (session.status !== "authenticated") {
+    return (
+      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <IconHoverEffect red>
+        <HeartIcon
+          className={`duration-200, transition-colors ${
+            likedByMe
+              ? "fill-red-500"
+              : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+          }`}
+        />
+      </IconHoverEffect>
+      <span>{likeCount}</span>
+    </button>
+  );
 }
