46a7d310a977b194f62c4ed40cf2f589334e9027
colineckert <colineckert10@gmail.com>
2024-01-01 09:38:33 -0800

    remove unneeded type def and refactor

--- src/components/InfinitePostList.tsx
+++ src/components/InfinitePostList.tsx
@@ -57,8 +57,10 @@ export function InfinitePostList({
         hasMore={hasMore}
         loader={<LoadingSpinner />}
       >
+        {/* TODO: remove this test post */}
         <GameRecapListItem
           key="test-recap"
+          type="GAME_RECAP"
           user={{
             id: "test",
             image: null,
@@ -174,72 +176,6 @@ function PostListItem({
   );
 }
 
-type HeartButtonProps = {
-  onClick: () => void;
-  isLoading: boolean;
-  likedByMe: boolean;
-  likeCount: number;
-};
-
-function HearButton({
-  onClick,
-  isLoading,
-  likedByMe,
-  likeCount,
-}: HeartButtonProps) {
-  const session = useSession();
-  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
-
-  if (session.status !== "authenticated") {
-    return (
-      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
-        <HeartIcon />
-        <span>{likeCount}</span>
-      </div>
-    );
-  }
-
-  return (
-    <button
-      disabled={isLoading}
-      onClick={onClick}
-      className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
-        likedByMe
-          ? "text-red-500"
-          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
-      }`}
-    >
-      <IconHoverEffect red>
-        <HeartIcon
-          className={`duration-200, transition-colors ${
-            likedByMe
-              ? "fill-red-500"
-              : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
-          }`}
-        />
-      </IconHoverEffect>
-      <span>{likeCount}</span>
-    </button>
-  );
-}
-
-type GameRecapListItemProps = {
-  id: string;
-  homeTeam: string;
-  awayTeam: string;
-  homeTeamScore: number;
-  awayTeamScore: number;
-  content: string;
-  createdAt: Date;
-  likeCount: number;
-  likedByMe: boolean;
-  user: {
-    id: string;
-    image: string | null;
-    name: string | null;
-  };
-};
-
 function GameRecapListItem({
   id,
   user,
@@ -250,7 +186,7 @@ function GameRecapListItem({
   createdAt,
   likeCount,
   likedByMe,
-}: GameRecapListItemProps) {
+}: Post) {
   const trpcUtils = api.useUtils();
   const toggleLike = api.post.toggleLike.useMutation({
     onSuccess: async ({ addedLike }) => {
@@ -341,3 +277,53 @@ function GameRecapListItem({
     </li>
   );
 }
+
+type HeartButtonProps = {
+  onClick: () => void;
+  isLoading: boolean;
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HearButton({
+  onClick,
+  isLoading,
+  likedByMe,
+  likeCount,
+}: HeartButtonProps) {
+  const session = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (session.status !== "authenticated") {
+    return (
+      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      disabled={isLoading}
+      onClick={onClick}
+      className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <IconHoverEffect red>
+        <HeartIcon
+          className={`duration-200, transition-colors ${
+            likedByMe
+              ? "fill-red-500"
+              : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+          }`}
+        />
+      </IconHoverEffect>
+      <span>{likeCount}</span>
+    </button>
+  );
+}
+
