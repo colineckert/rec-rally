d69e55f69511b3beae1904d77841bd5f505a434e
colineckert <colineckert10@gmail.com>
2024-01-14 19:47:57 -0800

    style side nav icons when selected

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,21 +1,28 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { IconHoverEffect } from "./IconHoverEffect";
-import { HiHome, HiUserCircle, HiUserGroup, HiLogin, HiLogout } from "react-icons/hi";
+import {
+  HiOutlineHome,
+  HiHome,
+  HiOutlineUserCircle,
+  HiUserCircle,
+  HiOutlineUsers,
+  HiUsers,
+  HiOutlineUserGroup,
+  HiUserGroup,
+  HiLogin,
+  HiLogout,
+} from "react-icons/hi";
 import { useRouter } from "next/router";
 
 export function SideNav() {
   const session = useSession();
   const user = session.data?.user;
-  const { pathname } = useRouter()
+  const { pathname } = useRouter();
 
   const isSelected = (path: string) => {
-    let boldStyles = '';
-    if (pathname === path) {
-      boldStyles = 'font-bold';
-    }
-    return boldStyles;
-  }
+    return pathname === path;
+  };
 
   return (
     <nav className="sticky top-0 px-4 py-4">
@@ -23,36 +30,74 @@ export function SideNav() {
         <li>
           <Link href="/">
             <IconHoverEffect>
-              <span className={`flex items-center gap-4 ${isSelected('/')}`}>
-                <HiHome className="h-8 w-8" />
+              <span
+                className={`flex items-center gap-4 ${
+                  isSelected("/") ? "font-bold" : ""
+                }`}
+              >
+                {isSelected('/') ? <HiHome className="h-8 w-8" /> : <HiOutlineHome className="h-8 w-8" />}
                 <span className="hidden text-lg md:inline">Home</span>
               </span>
             </IconHoverEffect>
           </Link>
         </li>
         {user != null && (
-          <li>
-            <Link href={`/profiles/${user.id}`}>
-              <IconHoverEffect>
-                <span className={`flex items-center gap-4 ${isSelected('/profiles/[id]')}`}>
-                  <HiUserCircle className="h-8 w-8" />
-                  <span className="hidden text-lg md:inline">Profile</span>
-                </span>
-              </IconHoverEffect>
-            </Link>
-          </li>
-        )}
-        {user != null && (
-          <li>
-            <Link href={`/teams`}>
-              <IconHoverEffect>
-                <span className={`flex items-center gap-4 ${isSelected('/teams')}`}>
-                  <HiUserGroup className="h-8 w-8" />
-                  <span className="hidden text-lg md:inline">Teams</span>
-                </span>
-              </IconHoverEffect>
-            </Link>
-          </li>
+          <>
+            <li>
+              <Link href={`/profiles/${user.id}`}>
+                <IconHoverEffect>
+                  <span
+                    className={`flex items-center gap-4 ${
+                      isSelected("/profiles/[id]") ? "font-bold" : ""
+                    }`}
+                  >
+                    {isSelected("/profiles/[id]") ? (
+                      <HiUserCircle className="h-8 w-8" />
+                    ) : (
+                      <HiOutlineUserCircle className="h-8 w-8" />
+                    )}
+                    <span className="hidden text-lg md:inline">Profile</span>
+                  </span>
+                </IconHoverEffect>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/teams`}>
+                <IconHoverEffect>
+                  <span
+                    className={`flex items-center gap-4 ${
+                      isSelected("/teams") ? "font-bold" : ""
+                    }`}
+                  >
+                    {isSelected("/teams") ? (
+                      <HiUsers className="h-8 w-8" />
+                    ) : (
+                      <HiOutlineUsers className="h-8 w-8" />
+                    )}
+                    <span className="hidden text-lg md:inline">Teams</span>
+                  </span>
+                </IconHoverEffect>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/leagues`}>
+                <IconHoverEffect>
+                  <span
+                    className={`flex items-center gap-4 ${
+                      isSelected("/leagues") ? "font-bold" : ""
+                    }`}
+                  >
+                    {isSelected("/leagues") ? (
+                      <HiUserGroup className="h-8 w-8" />
+                    ) : (
+                      <HiOutlineUserGroup className="h-8 w-8" />
+                    )}
+                    <span className="hidden text-lg md:inline">Leagues</span>
+                  </span>
+                </IconHoverEffect>
+              </Link>
+            </li>
+          </>
         )}
         {user == null ? (
           <li>
