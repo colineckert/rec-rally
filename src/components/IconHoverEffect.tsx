144f4cd68c9e5f960afe1fe92adc2dae0dcfafbe
colineckert <colineckert10@gmail.com>
2024-01-26 20:10:38 -0800

    switch to rounded-full for hover effect

--- src/components/IconHoverEffect.tsx
+++ src/components/IconHoverEffect.tsx
@@ -10,11 +10,13 @@ export function IconHoverEffect({
   red = false,
 }: IconHoverEffectProps) {
   const colorClasses = red
-    ? "rounded-full p-2 outline-red-400 hover:bg-red-200 group-hover-bg-red-200 group-focus-visible:bg-red-200"
-    : "rounded-lg px-3 py-2 outline-slate-400 hover:bg-slate-100 group-hover-bg-slate-100 group-focus-visible:bg-slate-100";
+    ? "p-2 outline-red-400 hover:bg-red-200 group-hover-bg-red-200 group-focus-visible:bg-red-200"
+    : "px-3 py-2 outline-slate-400 hover:bg-slate-100 group-hover-bg-slate-100 group-focus-visible:bg-slate-100";
 
   return (
-    <div className={`transition-colors duration-200 ${colorClasses}`}>
+    <div
+      className={`rounded-full transition-colors duration-200 ${colorClasses}`}
+    >
       {children}
     </div>
   );
