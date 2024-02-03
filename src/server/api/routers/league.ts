b1668ab63ee29380d205ef6fe5f0b831f81b5743
colineckert <colineckert10@gmail.com>
2024-02-02 22:04:11 -0800

    write get league by player procedure

--- src/server/api/routers/league.ts
+++ src/server/api/routers/league.ts
@@ -66,4 +66,69 @@ export const leagueRouter = createTRPCRouter({
         })),
       };
     }),
+
+  getByPlayerId: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input: { userId }, ctx }) => {
+      const leagues = await ctx.db.league.findMany({
+        where: {
+          players: {
+            some: {
+              id: userId,
+            },
+          },
+        },
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          _count: { select: { teams: true } },
+          teams: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+              manager: {
+                select: {
+                  id: true,
+                  name: true,
+                  image: true,
+                },
+              },
+            },
+          },
+          players: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            },
+          },
+        },
+      });
+
+      if (!leagues || leagues.length === 0) return [];
+
+      return leagues.map((league) => ({
+        id: league.id,
+        name: league.name,
+        description: league.description,
+        teamsCount: league._count.teams,
+        teams: league.teams.map((team) => ({
+          id: team.id,
+          name: team.name,
+          image: team.image,
+          manager: {
+            id: team.manager.id,
+            name: team.manager.name,
+            image: team.manager.image,
+          },
+        })),
+        players: league.players.map((player) => ({
+          id: player.id,
+          name: player.name,
+          image: player.image,
+        })),
+      }));
+    }),
 });
