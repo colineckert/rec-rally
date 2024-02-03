22531f451f07485cea4827a4ad4c4db4ec8e02a3
colineckert <colineckert10@gmail.com>
2024-02-03 15:24:56 -0800

    remove commented code

--- src/pages/leagues/index.tsx
+++ src/pages/leagues/index.tsx
@@ -48,24 +48,6 @@ const LeaguesPage: NextPage = (): JSX.Element => {
       <main>
         <div className="grid-row-3 mb-2 grid auto-rows-fr gap-6 border-b p-6 sm:grid-cols-3">
           <PlayerLeagues userId={user.id} />
-          {/* <div className="px-2">
-            <h3 className="pb-2 text-lg font-bold">Invites</h3>
-            <ul>
-              <li className="my-2 rounded-md border hover:bg-slate-100">
-                <div className="flex items-center justify-between p-2">
-                  <Link className="flex-grow pl-1" href={`/teams/1`}>
-                    Chelsea
-                  </Link>
-                  <Button className="mr-2">
-                    <HiCheck />
-                  </Button>
-                  <Button className="bg-red-500 p-1 hover:bg-red-400 focus-visible:bg-red-400">
-                    <HiX />
-                  </Button>
-                </div>
-              </li>
-            </ul>
-          </div> */}
         </div>
         {/* TODO: implement league posts */}
         <MyTeamsPosts />
@@ -78,53 +60,6 @@ const LeaguesPage: NextPage = (): JSX.Element => {
     </>
   );
 };
-
-// function ManagedTeams({ userId }: { userId: string }) {
-//   const {
-//     data: managedTeams,
-//     isLoading,
-//     isFetching,
-//   } = api.team.getManagerTeamsByUserId.useQuery({ userId });
-
-//   if (isLoading || isFetching) {
-//     return <LoadingSpinner />;
-//   }
-
-//   return (
-//     <div className="flex flex-col px-2 pb-4">
-//       <h3 className="pb-2 text-lg font-bold">Managed Teams</h3>
-//       <ul role="list">
-//         {managedTeams?.map((team) => (
-//           <li
-//             key={team.id}
-//             className="group/item my-2 rounded-md border hover:bg-slate-100"
-//           >
-//             <div className="flex items-center justify-between p-2">
-//               <ProfileImage
-//                 src={team.image}
-//                 className="mr-3 h-8 w-8 flex-shrink-0"
-//               />
-//               <div className="flex-grow">
-//                 <span>{team.name}</span>
-//                 <p className="text-sm text-gray-500">Manager: You</p>
-//               </div>
-//               <Link
-//                 href={`/teams/${team.id}`}
-//                 className="group/edit invisible flex items-center rounded-full p-2 text-gray-500 hover:bg-slate-200 group-hover/item:visible"
-//               >
-//                 {/* TODO: optimize for mobile */}
-//                 <span className="text-sm group-hover/edit:text-gray-700">
-//                   Manage
-//                 </span>
-//                 <HiChevronRight className="group-hover/edit:translate-x-0.5 group-hover/edit:text-slate-500" />
-//               </Link>
-//             </div>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
 
 function PlayerLeagues({ userId }: { userId: string }) {
   const {
