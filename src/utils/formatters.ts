831bce0e0109dcc3a9900cce1b810e95bf447e0c
colineckert <colineckert10@gmail.com>
2024-02-18 09:29:43 -0800

    migrate date formatter to util

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,3 +2,9 @@ const pluralRules = new Intl.PluralRules();
 export function getPlural(number: number, singular: string, plural: string) {
   return pluralRules.select(number) === "one" ? singular : plural;
 }
+
+export function getFormattedDate(date: Date, type: "short" | "long") {
+  return new Intl.DateTimeFormat(undefined, {
+    dateStyle: type,
+  }).format(date);
+}
