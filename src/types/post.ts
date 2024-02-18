dcbb3f7bcf2a0587bd4f46602b5f4c18e8a869ac
colineckert <colineckert10@gmail.com>
2024-02-18 09:28:46 -0800

    create post type file

---
+++ src/types/post.ts
@@ -0,0 +1,17 @@
+export type Post = {
+  id: string;
+  content: string;
+  type: "SOCIAL" | "GAME_RECAP" | "GAME_INVITE";
+  createdAt: Date;
+  likeCount: number;
+  likedByMe: boolean;
+  user: {
+    id: string;
+    image: string | null;
+    name: string | null;
+  };
+  homeTeamId: string | null;
+  awayTeamId: string | null;
+  homeScore: number | null;
+  awayScore: number | null;
+};
