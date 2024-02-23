cca4762f577f6b1f0cb83b85d6acda6a0d8d58f2
colineckert <colineckert10@gmail.com>
2024-02-23 07:11:35 -0800

    wip league edit modal

---
+++ src/components/league-modal/EditLeague.tsx
@@ -0,0 +1,160 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dialog, Listbox, Transition } from "@headlessui/react";
+import { Fragment, type SyntheticEvent, useState } from "react";
+import { api } from "~/utils/api";
+import type { inferProcedureOutput } from "@trpc/server";
+import type { AppRouter } from "~/server/api/root";
+import { HiChevronUpDown } from "react-icons/hi2";
+import { HiCheck } from "react-icons/hi";
+import type { League } from "@prisma/client";
+
+type EditLeagueModalProps = {
+  league: inferProcedureOutput<AppRouter["league"]["getById"]>;
+  isOpen: boolean;
+  closeModal: () => void;
+};
+
+export default function EditLeagueModal({
+  league,
+  isOpen,
+  closeModal,
+}: EditLeagueModalProps) {
+  const [name, setName] = useState(league?.name);
+  const [description, setDescription] = useState(league?.description);
+
+  if (!league?.id) return null;
+
+  // const editleague = api.league.update.useMutation({
+  //   onSuccess: async (updatedleague: League) => {
+  //     await trpcUtils.league.getById.invalidate({ id: updatedleague.id });
+  //   },
+  // });
+
+  async function handleUpdateleague(event: SyntheticEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!league) return;
+
+    const name = (event.target as any).leagueName.value;
+    const description = (event.target as any).description.value || null;
+    console.log("Update League", { name, description });
+    // await editleague.mutateAsync({
+    //   ...league,
+    //   name,
+    //   description,
+    //   leagueId: league?.id ?? null,
+    // });
+    closeModal();
+  }
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Edit League
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Update your league information.
+                  </p>
+                </div>
+                <form id="league-edit-form" onSubmit={handleUpdateleague}>
+                  <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
+                        <div className="sm:col-span-4">
+                          <label
+                            htmlFor="leagueName"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Name
+                          </label>
+                          <div className="mt-2">
+                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600 sm:max-w-md">
+                              <input
+                                required
+                                type="text"
+                                name="leagueName"
+                                id="leagueName"
+                                className="block flex-1 border-0 bg-transparent py-2 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-span-full">
+                          <label
+                            htmlFor="description"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Description
+                          </label>
+                          <div className="mt-2">
+                            <textarea
+                              id="description"
+                              name="description"
+                              rows={3}
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                              value={description ?? ""}
+                              onChange={(e) => setDescription(e.target.value)}
+                            />
+                          </div>
+                          <p className="mt-3 text-sm leading-6 text-gray-600">
+                            Write a few sentences about your league.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                      <button
+                        type="button"
+                        onClick={closeModal}
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        // disabled={editleague.isLoading}
+                        className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
