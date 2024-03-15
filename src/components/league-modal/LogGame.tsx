6c849e638029f59e995c33718850cbb33d975b89
colineckert <colineckert10@gmail.com>
2024-03-15 07:14:54 -0700

    start LogGameModal

--- src/components/league-modal/LogGame.tsx
+++ src/components/league-modal/LogGame.tsx
@@ -5,8 +5,27 @@ export default function LogGame({ leagueId }: { leagueId: string }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Button gray onClick={() => setIsOpen(true)}>
-      Log Game
-    </Button>
+    <>
+      <Button gray onClick={() => setIsOpen(true)}>
+        Log Game
+      </Button>
+      <LogGameModal
+        leagueId={leagueId}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+    </>
   );
+}
+
+function LogGameModal({
+  leagueId,
+  isOpen,
+  onClose,
+}: {
+  leagueId: string;
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return null;
 }
