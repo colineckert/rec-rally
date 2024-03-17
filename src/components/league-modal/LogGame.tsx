da5369ab9b0956dfef7cedf50422d1a2754815bc
colineckert <colineckert10@gmail.com>
2024-03-16 20:30:41 -0700

    wip log game modal layout

--- src/components/league-modal/LogGame.tsx
+++ src/components/league-modal/LogGame.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, type SyntheticEvent, useState } from "react";
 import { Button } from "../Button";
 
 export default function LogGame({ leagueId }: { leagueId: string }) {
@@ -12,7 +13,7 @@ export default function LogGame({ leagueId }: { leagueId: string }) {
       <LogGameModal
         leagueId={leagueId}
         isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
+        closeModal={() => setIsOpen(false)}
       />
     </>
   );
@@ -21,11 +22,79 @@ export default function LogGame({ leagueId }: { leagueId: string }) {
 function LogGameModal({
   leagueId,
   isOpen,
-  onClose,
+  closeModal,
 }: {
   leagueId: string;
   isOpen: boolean;
-  onClose: () => void;
+  closeModal: () => void;
 }) {
-  return null;
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Log Game
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Submit a game for this league but entering a home and away
+                    team and the score.
+                  </p>
+                </div>
+                <form id="log-game-form" onSubmit={() => null}>
+                  <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6"></div>
+                    </div>
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                      <button
+                        type="button"
+                        onClick={closeModal}
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        // disabled={editleague.isLoading}
+                        className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
 }
