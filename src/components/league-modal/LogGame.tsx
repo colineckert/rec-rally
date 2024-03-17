f9e659ec5f0b69297273c4b0ab39fd1cad7e521d
colineckert <colineckert10@gmail.com>
2024-03-17 07:56:55 -0700

    style log game modal

--- src/components/league-modal/LogGame.tsx
+++ src/components/league-modal/LogGame.tsx
@@ -1,8 +1,11 @@
-import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, type SyntheticEvent, useState } from "react";
-import { Button } from "../Button";
 import type { inferProcedureOutput } from "@trpc/server";
 import type { AppRouter } from "~/server/api/root";
+import { api } from "~/utils/api";
+import { HiCheck } from "react-icons/hi";
+import { HiChevronUpDown } from "react-icons/hi2";
+import { Dialog, Listbox, Transition } from "@headlessui/react";
+import { Button } from "../Button";
 
 type LogGameProps = {
   league: inferProcedureOutput<AppRouter["league"]["getById"]>;
@@ -44,21 +47,53 @@ function LogGameModal({
   isOpen,
   closeModal,
 }: LogGameModalProps) {
-  const [homeTeam, setHomeTeam] = useState("");
-  const [awayTeam, setAwayTeam] = useState("");
+  const [homeTeam, setHomeTeam] = useState<{
+    id: string;
+    name: string;
+  } | null>(null);
+  const [awayTeam, setAwayTeam] = useState<{
+    id: string;
+    name: string;
+  } | null>(null);
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
-  console.log("teams", teams);
+  const trpcUtils = api.useUtils();
+  const createGame = api.game.create.useMutation({
+    onSuccess: async () => {
+      await trpcUtils.post.infiniteLeagueFeed.invalidate({ leagueId });
+    },
+  });
 
-  const handleSubmit = (e: SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
+
+    if (!homeTeam || !awayTeam) return;
+
     console.log({ homeTeam, awayTeam, homeScore, awayScore });
+
+    // await createGame.mutateAsync({
+    //   date: new Date(),
+    //   homeTeamId: homeTeam.id,
+    //   awayTeamId: awayTeam.id,
+    //   homeScore,
+    //   awayScore,
+    //   leagueId,
+    //   friendly: false,
+    // });
+  };
+
+  const handleClose = () => {
+    closeModal();
+    setHomeTeam(null);
+    setAwayTeam(null);
+    setHomeScore(0);
+    setAwayScore(0);
   };
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+      <Dialog as="div" className="relative z-10" onClose={handleClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -82,7 +117,7 @@ function LogGameModal({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full max-w-lg transform rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
@@ -92,25 +127,194 @@ function LogGameModal({
                 <div className="mt-2">
                   <p className="text-sm text-gray-500">
                     Submit a game for this league but entering a home and away
-                    team and the score.
+                    team and the game's final score.
                   </p>
                 </div>
                 <form id="log-game-form" onSubmit={handleSubmit}>
                   <div className="space-y-12">
-                    <div className="border-b border-gray-900/10 pb-12">
-                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6"></div>
+                    <div className="border-b border-gray-900/10 pb-10">
+                      <div className="mt-6">
+                        <Listbox value={homeTeam} onChange={setHomeTeam}>
+                          <Dialog.Title
+                            as="h5"
+                            className="text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Home
+                          </Dialog.Title>
+                          <div className="relative mt-1 flex gap-4">
+                            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-green-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                              <span className="block truncate">
+                                {homeTeam?.name}
+                              </span>
+                              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                <HiChevronUpDown
+                                  className="h-5 w-5 text-gray-400"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            </Listbox.Button>
+                            <Transition
+                              as={Fragment}
+                              leave="transition ease-in duration-100"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0"
+                            >
+                              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                                {teams.map((team, teamIdx) => (
+                                  <Listbox.Option
+                                    key={teamIdx}
+                                    className={({ active }) =>
+                                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                        active
+                                          ? "bg-green-100 text-green-900"
+                                          : "text-gray-900"
+                                      }`
+                                    }
+                                    value={team}
+                                  >
+                                    {({ selected }) => (
+                                      <>
+                                        <span
+                                          className={`block truncate ${
+                                            selected
+                                              ? "font-medium"
+                                              : "font-normal"
+                                          }`}
+                                        >
+                                          {team.name}
+                                        </span>
+                                        {selected ? (
+                                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
+                                            <HiCheck
+                                              className="h-5 w-5"
+                                              aria-hidden="true"
+                                            />
+                                          </span>
+                                        ) : null}
+                                      </>
+                                    )}
+                                  </Listbox.Option>
+                                ))}
+                              </Listbox.Options>
+                            </Transition>
+                            <div className="rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600">
+                              <input
+                                required
+                                type="number"
+                                name="homeScore"
+                                id="homeScore"
+                                className="block flex-1 border-0 bg-transparent py-2 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                value={homeScore}
+                                onChange={(e) =>
+                                  setHomeScore(parseInt(e.target.value, 10))
+                                }
+                              />
+                            </div>
+                          </div>
+                        </Listbox>
+                      </div>
+                      <div className="mt-6">
+                        <Listbox value={awayTeam} onChange={setAwayTeam}>
+                          <Dialog.Title
+                            as="h5"
+                            className="text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Away
+                          </Dialog.Title>
+                          <div className="relative mt-1 flex gap-4">
+                            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-green-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                              <span className="block truncate">
+                                {awayTeam?.name}
+                              </span>
+                              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                <HiChevronUpDown
+                                  className="h-5 w-5 text-gray-400"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            </Listbox.Button>
+                            <Transition
+                              as={Fragment}
+                              leave="transition ease-in duration-100"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0"
+                            >
+                              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                                {teams.map((team, teamIdx) => (
+                                  <Listbox.Option
+                                    key={teamIdx}
+                                    className={({ active }) =>
+                                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                        active
+                                          ? "bg-green-100 text-green-900"
+                                          : "text-gray-900"
+                                      }`
+                                    }
+                                    value={team}
+                                  >
+                                    {({ selected }) => (
+                                      <>
+                                        <span
+                                          className={`block truncate ${
+                                            selected
+                                              ? "font-medium"
+                                              : "font-normal"
+                                          }`}
+                                        >
+                                          {team.name}
+                                        </span>
+                                        {selected ? (
+                                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
+                                            <HiCheck
+                                              className="h-5 w-5"
+                                              aria-hidden="true"
+                                            />
+                                          </span>
+                                        ) : null}
+                                      </>
+                                    )}
+                                  </Listbox.Option>
+                                ))}
+                              </Listbox.Options>
+                            </Transition>
+                            <div className="rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-green-600">
+                              <input
+                                required
+                                type="number"
+                                name="awayScore"
+                                id="awayScore"
+                                className="block flex-1 border-0 bg-transparent py-2 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                value={awayScore}
+                                onChange={(e) =>
+                                  setAwayScore(parseInt(e.target.value, 10))
+                                }
+                              />
+                            </div>
+                          </div>
+                        </Listbox>
+                      </div>
+                      {homeTeam && awayTeam && homeTeam.id === awayTeam.id && (
+                        <p className="mt-4 text-sm text-red-500">
+                          Home and away teams cannot be the same.
+                        </p>
+                      )}
                     </div>
                     <div className="mt-6 flex items-center justify-end gap-x-6">
                       <button
                         type="button"
-                        onClick={closeModal}
+                        onClick={handleClose}
                         className="text-sm font-semibold leading-6 text-gray-900"
                       >
                         Cancel
                       </button>
                       <button
                         type="submit"
-                        // disabled={editleague.isLoading}
+                        disabled={
+                          createGame.isLoading ||
+                          !homeTeam ||
+                          !awayTeam ||
+                          homeTeam.id === awayTeam.id
+                        }
                         className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                       >
                         Save
