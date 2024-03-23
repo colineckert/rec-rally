853343de7eba8bdf5b133579b4d46ad63bc67bac
colineckert <colineckert10@gmail.com>
2024-03-23 07:08:40 -0700

    getTeamById procedure

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -33,6 +33,34 @@ export const gameRouter = createTRPCRouter({
         friendly: game.friendly,
       }));
     }),
+  getByTeamId: publicProcedure
+    .input(z.object({ teamId: z.string() }))
+    .query(async ({ input: { teamId }, ctx }) => {
+      const games = await ctx.db.game.findMany({
+        where: {
+          OR: [{ homeTeamId: teamId }, { awayTeamId: teamId }],
+        },
+        select: {
+          id: true,
+          date: true,
+          homeTeamId: true,
+          awayTeamId: true,
+          homeScore: true,
+          awayScore: true,
+          friendly: true,
+        },
+      });
+
+      return games.map((game) => ({
+        id: game.id,
+        date: game.date,
+        homeTeamId: game.homeTeamId,
+        awayTeamId: game.awayTeamId,
+        homeScore: game.homeScore,
+        awayScore: game.awayScore,
+        friendly: game.friendly,
+      }));
+    }),
   getById: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input: { id }, ctx }) => {
