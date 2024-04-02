61e0d41262b2bae329530fa25d1a7578e4adf503
colineckert <colineckert10@gmail.com>
2024-04-02 06:31:37 -0700

    use combobox for game invite team select

--- src/components/team-modal/GameInvite.tsx
+++ src/components/team-modal/GameInvite.tsx
@@ -4,8 +4,9 @@ import type { AppRouter } from "~/server/api/root";
 import { api } from "~/utils/api";
 import { HiCheck } from "react-icons/hi";
 import { HiChevronUpDown } from "react-icons/hi2";
-import { Dialog, Listbox, Switch, Transition } from "@headlessui/react";
+import { Combobox, Dialog, Switch, Transition } from "@headlessui/react";
 import { Button } from "../Button";
+import type { Team } from "@prisma/client";
 
 type LogGameProps = {
   team: inferProcedureOutput<AppRouter["team"]["getById"]>;
@@ -37,10 +38,7 @@ type GameInviteModalProps = {
 };
 
 type GameInviteFormValues = {
-  awayTeam: {
-    id: string;
-    name: string;
-  } | null;
+  awayTeam: Team | null;
   date: Date | null;
   friendly: boolean;
 };
@@ -52,8 +50,6 @@ const defaultFormValues: GameInviteFormValues = {
 };
 
 // TODO:
-// - Select league
-// - On league select, fetch teams
 // - Select opponent team
 // - Select date
 // - Friendly switch
@@ -67,17 +63,31 @@ function GameInviteModal({
   closeModal,
 }: GameInviteModalProps) {
   const [formValues, setFormValues] = useState(defaultFormValues);
-  const [teams, setTeams] = useState<{ id: string; name: string }[] | null>(
-    null,
-  );
+  const [query, setQuery] = useState("");
 
-  const trpcUtils = api.useUtils();
-  const createGame = api.post.create.useMutation();
+  const { data: teams } = api.team.getAll.useQuery();
+
+  const opponents = teams?.filter((team) => team.id !== homeTeam?.id);
+  const filteredTeams =
+    query === ""
+      ? opponents
+      : opponents?.filter((team) =>
+          team.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, "")),
+        );
+
+  // const createGame = api.post.create.useMutation();
 
   const handleClose = () => {
     closeModal();
     setFormValues(defaultFormValues);
   };
+
+  function handleTeamSelect(team: Team) {
+    setFormValues((prev) => ({ ...prev, awayTeam: team }));
+  }
 
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -142,83 +152,99 @@ function GameInviteModal({
                   </p>
                 </div>
                 <form id="log-game-form" onSubmit={handleSubmit}>
-                  <div className="space-y-12">
-                    <div className="border-b border-gray-900/10 pb-10">
-                      <div className="mt-6">
-                        <Listbox
-                          value={formValues.awayTeam}
-                          onChange={(team) =>
-                            setFormValues((prev) => ({
-                              ...prev,
-                              awayTeam: team,
-                            }))
-                          }
-                        >
-                          <Dialog.Title
-                            as="h5"
-                            className="text-sm font-medium leading-6 text-gray-900"
-                          >
-                            Opponent
-                          </Dialog.Title>
+                  <div className="mt-10 grid grid-cols-1 gap-x-4 gap-y-4">
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="players"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Select Opponent
+                      </label>
+                      <div className="mt-2">
+                        <Combobox onChange={handleTeamSelect}>
                           <div className="relative mt-1">
-                            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-green-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                              <span className="block truncate">
-                                {formValues.awayTeam?.name}
-                              </span>
-                              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                            <div className="relative w-full cursor-default rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-green-300 sm:text-sm">
+                              <Combobox.Input
+                                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                                placeholder="Search teams..."
+                                displayValue={(team: Team) => team?.name ?? ""}
+                                onChange={(event) =>
+                                  setQuery(event.target.value)
+                                }
+                              />
+                              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
                                 <HiChevronUpDown
                                   className="h-5 w-5 text-gray-400"
                                   aria-hidden="true"
                                 />
-                              </span>
-                            </Listbox.Button>
+                              </Combobox.Button>
+                            </div>
                             <Transition
                               as={Fragment}
                               leave="transition ease-in duration-100"
                               leaveFrom="opacity-100"
                               leaveTo="opacity-0"
+                              afterLeave={() => setQuery("")}
                             >
-                              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                                {teams?.map((team, teamIdx) => (
-                                  <Listbox.Option
-                                    key={teamIdx}
-                                    className={({ active }) =>
-                                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                        active
-                                          ? "bg-green-100 text-green-900"
-                                          : "text-gray-900"
-                                      }`
-                                    }
-                                    value={team}
-                                  >
-                                    {({ selected }) => (
-                                      <>
-                                        <span
-                                          className={`block truncate ${
-                                            selected
-                                              ? "font-medium"
-                                              : "font-normal"
-                                          }`}
-                                        >
-                                          {team.name}
-                                        </span>
-                                        {selected ? (
-                                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-600">
-                                            <HiCheck
-                                              className="h-5 w-5"
-                                              aria-hidden="true"
-                                            />
-                                          </span>
-                                        ) : null}
-                                      </>
-                                    )}
-                                  </Listbox.Option>
-                                ))}
-                              </Listbox.Options>
+                              <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                                {filteredTeams?.length === 0 && query !== "" ? (
+                                  <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+                                    No matches.
+                                  </div>
+                                ) : (
+                                  filteredTeams?.map((team) => (
+                                    <Combobox.Option
+                                      key={team.id}
+                                      className={({ active }) =>
+                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                          active
+                                            ? "bg-green-100 text-green-900"
+                                            : "text-gray-900"
+                                        }`
+                                      }
+                                      value={team}
+                                    >
+                                      {({ selected, active }) => {
+                                        return (
+                                          <>
+                                            <span
+                                              className={`block truncate ${
+                                                selected
+                                                  ? "font-medium"
+                                                  : "font-normal"
+                                              }`}
+                                            >
+                                              {team.name}
+                                            </span>
+                                            {selected ? (
+                                              <span
+                                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                  active
+                                                    ? "bg-green-100 text-green-900"
+                                                    : "text-gray-900"
+                                                }`}
+                                              >
+                                                <HiCheck
+                                                  className="h-5 w-5 text-green-500"
+                                                  aria-hidden="true"
+                                                />
+                                              </span>
+                                            ) : null}
+                                          </>
+                                        );
+                                      }}
+                                    </Combobox.Option>
+                                  ))
+                                )}
+                              </Combobox.Options>
                             </Transition>
                           </div>
-                        </Listbox>
+                        </Combobox>
                       </div>
+                    </div>
+                  </div>
+                  <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-10">
                       <div className="flex items-center gap-2 pt-6">
                         <Switch
                           checked={formValues.friendly}
@@ -259,7 +285,7 @@ function GameInviteModal({
                       </button>
                       <button
                         type="submit"
-                        disabled={createGame.isLoading || !formValues.awayTeam}
+                        // disabled={createGame.isLoading || !formValues.awayTeam}
                         className="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                       >
                         Save
